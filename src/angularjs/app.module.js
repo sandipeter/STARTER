5454c86ea353c95471b407d30344d8b0a9c68e2e
sandipeter <sandipeter83@gmail.com>
2018-01-04 01:32:07 +0100

    controller as

--- src/angularjs/app.module.js
+++ src/angularjs/app.module.js
@@ -1,7 +1,11 @@
-angular.module('expressionExample', [])
-.controller('ExampleController', ['$window', '$scope', function($window, $scope) {
-	$scope.name = 'World';
-	$scope.greet = function(){
-		$window.alert('valami');
-	}
-}]);
+var app = angular.module('app', []);
+
+app.controller('MainCtrl', function(){
+  var self = this;
+
+  self.message = 'hello';
+
+  self.changeMessage = function(message){
+    self.message = message;
+  };
+});
