569b4b82d8896f76620102640e54a86431575909
sandipeter <sandipeter83@gmail.com>
2018-01-04 04:18:11 +0100

    filters 2

--- src/angularjs/app.module.js
+++ src/angularjs/app.module.js
@@ -1,14 +1,30 @@
 function TestCtrl() {
-  var self = this;
-  self.myString = "hello world";
+	var self = this;
+	self.myString = "hello world";
+
+	self.people = [
+		{
+			name: "Eric Simons",
+			born: "Chicago"
+      },
+		{
+			name: "Albert Pai",
+			born: "Taiwan"
+      },
+		{
+			name: "Matthew Greenster",
+			born: "Virginia"
+      }
+    ];
+
 }
 
 function CapitalizeFilter() {
-	return function(text){
+	return function (text) {
 		return text.toUpperCase();
 	}
 }
 
 angular.module('app', [])
-.controller('TestCtrl', [TestCtrl])
-.filter('capitalize', CapitalizeFilter);
+	.controller('TestCtrl', [TestCtrl])
+	.filter('capitalize', CapitalizeFilter);
