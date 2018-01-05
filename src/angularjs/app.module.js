052bc082314b3dcc9f5377f7be77310d87737335
sandipeter <sandipeter83@gmail.com>
2018-01-05 01:02:07 +0100

    directives - tabset -> tabs

--- src/angularjs/app.module.js
+++ src/angularjs/app.module.js
@@ -1,11 +1,59 @@
-angular.module('greetings', [])
-	.directive("welcome", function () {
+;
+(function (window) {
+
+	var app = angular.module('app', [])
+
+	app.directive('tab', function () {
 		return {
-			restrict: "E",
-			scope: {},
+			restrict: 'E',
 			transclude: true,
-			template: '<div>This is the welcome component</div><ng-transclude></ng-transclude>'
+			template: '<div role="tabpanel" ng-show="active" ng-transclude></div>',
+			require: '^tabset',
+			scope: {
+				heading: '@'
+			},
+			link: function (scope, elem, attr, tabsetCtrl) {
+
+				scope.active = false;
+				tabsetCtrl.addTab(scope);
+			}
 		}
 	})
 
+	app.directive('tabset', function () {
+		return {
+			restrict: 'E',
+			transclude: true,
+			templateUrl: 'angularjs/views/tabset.html',
+			scope: {},
+			bindToController: true,
+			controllerAs: 'tabset',
+			controller: function(){
 
+				var self = this;
+				self.tabs = [];
+
+				self.addTab = function(tab){
+					self.tabs.push(tab);
+
+					if(self.tabs.length ===1){
+						tab.active = true;
+					}
+				}
+
+				self.select = function(selectedTab){
+					angular.forEach(self.tabs, function(tab){
+						if(tab.active && tab !== selectedTab){
+							tab.active = false
+						}
+						selectedTab.active = true;
+					})
+				}
+
+			}
+		}
+	})
+
+	// Define directives here
+
+})(window);
