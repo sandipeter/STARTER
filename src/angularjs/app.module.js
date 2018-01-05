c2e424b14db30f1ff5a444b1556ba4ae742d179f
sandipeter <sandipeter83@gmail.com>
2018-01-05 01:42:10 +0100

    directive - controller inheritance example

--- src/angularjs/app.module.js
+++ src/angularjs/app.module.js
@@ -1,59 +1,35 @@
-;
-(function (window) {
+var app = angular.module("app",[]);
 
-	var app = angular.module('app', [])
-
-	app.directive('tab', function () {
-		return {
-			restrict: 'E',
-			transclude: true,
-			template: '<div role="tabpanel" ng-show="active" ng-transclude></div>',
-			require: '^tabset',
-			scope: {
-				heading: '@'
-			},
-			link: function (scope, elem, attr, tabsetCtrl) {
-
-				scope.active = false;
-				tabsetCtrl.addTab(scope);
-			}
+app.directive("country", function () {
+  return {
+    restrict: 'E',
+	controller: function(){
+		this.makeAnnouncement = function(message){
+			console.log("Country says: " + message);
 		}
-	})
+	}
 
-	app.directive('tabset', function () {
-		return {
-			restrict: 'E',
-			transclude: true,
-			templateUrl: 'angularjs/views/tabset.html',
-			scope: {},
-			bindToController: true,
-			controllerAs: 'tabset',
-			controller: function(){
+  };
+});
 
-				var self = this;
-				self.tabs = [];
+app.directive("state", function () {
+  return {
+    restrict: "E",
+    controller: function () {
+      this.makeLaw = function (law) {
+        console.log("Law: " + law);
+      };
+    }
+  };
+});
 
-				self.addTab = function(tab){
-					self.tabs.push(tab);
-
-					if(self.tabs.length ===1){
-						tab.active = true;
-					}
-				}
-
-				self.select = function(selectedTab){
-					angular.forEach(self.tabs, function(tab){
-						if(tab.active && tab !== selectedTab){
-							tab.active = false
-						}
-						selectedTab.active = true;
-					})
-				}
-
-			}
-		}
-	})
-
-	// Define directives here
-
-})(window);
+app.directive("city", function () {
+  return {
+    restrict: "E",
+    require: ["^country","^state"],
+    link: function (scope, element, attrs, ctrls) {
+      ctrls[0].makeAnnouncement("This city rocks");
+      ctrls[1].makeLaw("Jump higher");
+    }
+  };
+});
