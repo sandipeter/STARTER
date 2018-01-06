753462e9ee2fc70ff4d4b60ae487e01f7a0da3dc
sandipeter <sandipeter83@gmail.com>
2018-01-06 01:45:48 +0100

    basic http

--- src/angularjs/app.module.js
+++ src/angularjs/app.module.js
@@ -1,28 +1,34 @@
-var app = angular.module('myApp', ['ngRoute'])
-
-app.config(['$routeProvider', function ($routeProvider) {
-	$routeProvider.when("/", {
-			templateUrl: "angularjs/views/phone.html",
-			controller: "AppCtrl",
-			controllerAs: "app"
-		}).when('/cookies', {
-			redirectTo: function (routeParams, path, search){
-				console.log(routeParams);
-				console.log(path);
-				console.log(search);
-				return "/" + routeParams.cookieType;
-			}
+function testService($http){
+	this.get = function(){
+		return $http.get('http://test-routes.herokuapp.com/test/hello')
+			.then(function(res){
+			return res.data.message;
 		})
-		.when('/sugar', {
-			template: 'Sugar cookie'
-		})
-		.otherwise({
-			redirectTo: '/'
-		})
-}]);
+	}
+	this.upperCase = function(data){
+		return $http.post('http://test-routes.herokuapp.com/test/uppercase', data)
+	}
+}
 
-app.controller('AppCtrl', ['$routeParams', function ($routeParams) {
-
+function TestCtrl(testService){
 	var self = this;
-	self.message = "The app routing is working!"
-}])
+	testService.get().then(function(res){
+		self.message = res;
+		console.log(res);
+	})
+
+	self.postData = function(message){
+		testService.upperCase({message: message})
+		.success(function(body){
+			self.sendMessage = body.message;
+		})
+	}
+
+}
+
+
+var app = angular.module('app', [])
+
+app.service('testService', ['$http', testService])
+app.controller('TestCtrl', ['testService', TestCtrl])
+
